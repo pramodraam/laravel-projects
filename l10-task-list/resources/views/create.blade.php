7d4045bbdcbc79f1f780498f48d833c60f09f34d
Pramod Kumar T G <pramodraam@gmail.com>
2024-02-26 18:07:24 +0530

    Error, session and flash msg

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
+@section('style')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0.8rem;
+        }
+    </style>
+@endsection
+
 @section('title', 'Add Tasks')
 
 @section('content')
@@ -9,16 +18,24 @@
         <label for="title">Title</label>
         <input text="text" name="title" id="title">
     </div>
-
+    @error('title')
+    <p class="error-message"> {{ $message }} </p>
+    @enderror
     <div>
         <label for="description">Description</label>
         <textarea name="description" id="description" rows="5"></textarea>
     </div>
+    @error('description')
+    <p class="error-message"> {{ $message }} </p>
+    @enderror
 
     <div>
         <label for="long_description">Long Description</label>
         <textarea name="long_description" id="long_description" rows="10"></textarea>
     </div>
+    @error('long_description')
+    <p class="error-message"> {{ $message }} </p>
+    @enderror
 
     <div>
         <button type="submit">Add Task</button>
--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -2,10 +2,16 @@
 <html>
     <head>
         <title>Laravel 10 Task List</title>
+        @yield('style')
     </head>
     <body>
         <h1>@yield('title')</h1>
         <div>
+            @if (session()->has('success'))
+                <div>
+                    {{ session('success') }}
+                </div>
+            @endif
             @yield('content')
         </div>
     </body>

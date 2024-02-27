4c1ff5a7002e4c930bb834e68c547beca6f03f43
Pramod Kumar T G <pramodraam@gmail.com>
2024-02-27 16:32:39 +0530

    Toggling state

--- l10-task-list/app/Models/Task.php
+++ l10-task-list/app/Models/Task.php
@@ -11,4 +11,9 @@ class Task extends Model
 
     protected $fillable = ['title','description','long_description'];
     //protected $guarded = ['password'];
+
+    public function toggleComplete() {
+        $this->completed = !$this->completed;
+        $this->save();
+    }
 }
--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -66,6 +66,12 @@ Route::delete('/tasks/{task}', function (Task $task) {
     ->with('success','Task Deleted Successfully');
 })->name('tasks.destroy');
 
+Route::put('/tasks/{task}/toggle-complete', function (Task $task) {
+  $task->toggleComplete();
+
+  return redirect()->back()->with('success','Task updated successfully!');
+})->name('tasks.toggle-complete');
+
 // Route::get('/xxx', function () { 
 //     return 'Hello';
 // })->name('hello');

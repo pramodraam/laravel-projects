d6ff479043860a265316efc3fb06d03953f72452
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-06 22:44:59 +0530

    Api Resources, loading relationships, controlling responses

--- event-management/app/Http/Controllers/api/EventController.php
+++ event-management/app/Http/Controllers/api/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return \App\Models\User::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -20,23 +22,44 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return new EventResource($event);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(\App\Models\Event $event)
-    {
-        return $event;
+    public function show(Event $event)
+    {   
+        $event->load('user', 'attendees');
+
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'sometimes|date',
+                'end_time' => 'sometimes|date|after:start_time'
+            ])
+        );
+
+        return new EventResource($event);
     }
 
     /**
@@ -44,6 +67,7 @@ class EventController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+      $event = Event::findOrFail($id);
+      $event->delete();
     }
 }

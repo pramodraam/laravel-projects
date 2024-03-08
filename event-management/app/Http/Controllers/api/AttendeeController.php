14c8427a82ce6cb501085b81d3589e0f092e8703
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-08 12:29:14 +0530

    Added Trait in AttendeeController as well

--- event-management/app/Http/Controllers/api/AttendeeController.php
+++ event-management/app/Http/Controllers/api/AttendeeController.php
@@ -7,15 +7,23 @@ use App\Http\Resources\AttendeeResource;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Attendee;
+use App\Http\Traits\CanLoadRelationships;
 
 class AttendeeController extends Controller
 {
+    
+    use CanLoadRelationships;
+    
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -27,9 +35,11 @@ class AttendeeController extends Controller
      */
     public function store(Request $request, Event $event)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 1
-        ]);
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
 
         return new AttendeeResource($attendee);
     }
@@ -39,7 +49,9 @@ class AttendeeController extends Controller
      */
     public function show(Event $event, Attendee $attendee)
     {
-       return new AttendeeResource($attendee);
+       return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**

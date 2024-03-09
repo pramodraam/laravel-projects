969b239448b1eeb2fee12c911d28ecedb8be2f5d
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-09 22:35:38 +0530

    Custom artisan command

---
+++ event-management/app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notifications to all attendees that the event is near!';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+        
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $this->info("Found {$eventCount} {$eventLabel}");
+
+        $events->each(
+            fn ($event) => $event->attendees->each(
+                fn ($attendee) => $this->info(
+                    "Sending notification to user {$attendee->user_id} ")
+                )
+            );
+
+
+        $this->info('Reminder notifications sent successfully!');
+    }
+}

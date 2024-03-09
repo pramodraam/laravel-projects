ad6c4590296b5fd70f9a7012292e33c400c73173
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-09 14:56:25 +0530

    Authorization with Gates

--- event-management/app/Providers/AuthServiceProvider.php
+++ event-management/app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Attendee;
+use App\Models\Event;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define("update-event", function ($user, Event $event) {
+            return $user->id === $event->user->id;
+        });
+
+        Gate::define("delete-event", function ($user, Event $event, Attendee $attendee) {
+            return $user->id === $attendee->user_id ||
+                $user->id === $event->user_id;
+        });
     }
 }

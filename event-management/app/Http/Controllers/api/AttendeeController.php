43fe8de46f717af6dd0e735373a7340f32648b49
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-10 00:47:07 +0530

    Rate limitter

--- event-management/app/Http/Controllers/api/AttendeeController.php
+++ event-management/app/Http/Controllers/api/AttendeeController.php
@@ -18,6 +18,7 @@ class AttendeeController extends Controller
 
     public function __construct() {
         $this->middleware('auth:sanctum')->except(['index','show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -42,7 +43,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
@@ -72,8 +73,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-        //$this->authorize('delete-event', [$event, $attendee]);
-
         $attendee->delete();
 
         return response(status: 204);

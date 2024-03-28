b6136b709d7d6f8c4668b9f0e488f76bef55cb66
Pramod Kumar T G <pramodraam@gmail.com>
2024-03-28 13:09:45 +0530

    Voting- creating records in votes table

--- livewire-poll/app/Livewire/Polls.php
+++ livewire-poll/app/Livewire/Polls.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Option;
 
 class Polls extends Component
 {
@@ -13,5 +14,10 @@ class Polls extends Component
     {
         $polls = \App\Models\Poll::with("options.votes")->latest()->get();
         return view('livewire.polls', ['polls'=> $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
